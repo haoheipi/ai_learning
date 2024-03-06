b40fa4a08231727ee425e86669795ef8d8e94b13
haoheipi <hellowuyuhao@gmail.com>
2024-03-07 00:40:19 +0800

    first commit

---
+++ langchain_learning/01_LangChain快速入门/02_ChatModel.py
@@ -0,0 +1,40 @@
+'''欢迎来到LangChain实战课
+https://time.geekbang.org/column/intro/100617601
+作者 黄佳'''
+
+from dotenv import load_dotenv  # 用于加载环境变量
+load_dotenv()  # 加载 .env 文件中的环境变量
+
+# import os
+# os.environ["OPENAI_API_KEY"] = '你的OpenAI API Key'
+
+# import openai
+
+# response = openai.ChatCompletion.create(
+#   model="gpt-4",
+#   messages=[
+#         {"role": "system", "content": "You are a creative AI."},
+#         {"role": "user", "content": "请给我的花店起个名"},
+#     ],
+#   temperature=0.8,
+#   max_tokens=60
+# )
+
+# print(response['choices'][0]['message']['content'])
+
+# print(response.choices)
+
+
+from openai import OpenAI
+client = OpenAI()
+
+response = client.completions.create(  model="gpt-4",
+  messages=[
+        {"role": "system", "content": "You are a creative AI."},
+        {"role": "user", "content": "请给我的花店起个名"},
+    ],
+  temperature=0.8,
+  max_tokens=60
+)
+
+print(response.choices[0].message.content)
